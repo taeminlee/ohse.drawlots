83948e0bf7df2bc6313fdc74da3cfe9098010056
persuade <persuade@gmail.com>
2017-03-08 20:56:07 +0900

    Added : icon

--- ohse.drawlots/ohse.drawlots/Properties/Resources.Designer.cs
+++ ohse.drawlots/ohse.drawlots/Properties/Resources.Designer.cs
@@ -70,6 +70,16 @@ namespace ohse.drawlots.Properties {
         }
         
         /// <summary>
+        ///   (아이콘)과(와) 유사한 System.Drawing.Icon 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static System.Drawing.Icon dice {
+            get {
+                object obj = ResourceManager.GetObject("dice", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   System.IO.MemoryStream과(와) 유사한 System.IO.UnmanagedMemoryStream 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
         internal static System.IO.UnmanagedMemoryStream tick {
